e30c18140f19fd3dbce99b7eecf04eb64bb2b34e
lhh108 <paintluis@gmail.com>
2019-09-18 22:52:57 +0000

    Territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+
+char hcar; 
+int integer;
+long int longint;
+long long int llint;
+float flt;
+
+printf("Size of a char: %d byte(s)\n", sizeof(hcar));
+printf("Size of an int: %d byte(s)\n", sizeof(integer));
+printf("Size of a long int: %d byte(s)\n", sizeof(longint));
+printf("Size of a long long int: %d byte(s)\n", sizeof(llint));
+printf("Size of a float: %d byte(s)\n", sizeof(flt));
+
+return (0);
+
+}
