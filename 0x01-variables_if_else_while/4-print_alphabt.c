d320be7773a61cbbd0b27eae749659553a2ddd1c
lhh108 <paintluis@gmail.com>
2019-09-20 02:12:48 +0000

    Adding print number

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  char dsa;
+
+  for (dsa = 97; dsa <= 122; dsa++)
+    putchar(dsa);
+  putchar(10);
+  return (0);
+}
