90037c523630e38e2b2470d0e5ca15de7179adb4
lhh108 <paintluis@gmail.com>
2019-09-20 01:33:20 +0000

    lower alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Desc: alphabet lowercase game
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+
+for (a = 'a'; a < 'z'; a++)
+putchar(a);
+putchar(10);
+return (0);
+}
