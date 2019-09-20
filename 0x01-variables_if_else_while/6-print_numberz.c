99a6cf37b51321920f5c4e15e6563e8666cea856
lhh108 <paintluis@gmail.com>
2019-09-20 02:23:56 +0000

    Removed Numberz executable

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int dsa;
+
+for (dsa = 48; dsa <= 57; dsa++)
+putchar(dsa);
+putchar(10);
+return (0);
+}
