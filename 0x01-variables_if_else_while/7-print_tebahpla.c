afbdd7e0493485e2c8aab089ebcac4a94f588c7e
lhh108 <paintluis@gmail.com>
2019-09-20 02:53:55 +0000

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char dsa;
+
+for (dsa = 'z'; dsa >= 'a'; dsa--)
+putchar(dsa);
+putchar('\n');
+return (0);
+}
