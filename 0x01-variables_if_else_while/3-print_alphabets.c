64467e5c09dbdde0afff42cf3f40430b92aeece4
lhh108 <paintluis@gmail.com>
2019-09-20 01:54:12 +0000

    Added wrong file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char dsa;
+
+for (dsa = 97; dsa <= 122; dsa++)
+putchar(dsa);
+for (dsa = 65; dsa <= 90; dsa++)
+putchar(dsa);
+putchar(10);
+return (0);
+}
