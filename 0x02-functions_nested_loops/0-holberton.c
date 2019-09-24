0766cdf850cd912d76e9a2aae5aa31696cffc508
lhh108 <paintluis@gmail.com>
2019-09-24 15:50:48 +0000

    Adding Nested loops

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ * Desc: Print Holberton without printf put
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int h = 72;
+	int o = 111;
+	int l = 108;
+	int b = 98;
+	int e = 101;
+	int r = 114;
+	int t = 116;
+	int n = 110;
+
+	_putchar(h);
+	_putchar(o);
+	_putchar(l);
+	_putchar(b);
+	_putchar(e);
+	_putchar(r);
+	_putchar(t);
+	_putchar(o);
+	_putchar(n);
+	_putchar(10);
+	return (0);
+}
