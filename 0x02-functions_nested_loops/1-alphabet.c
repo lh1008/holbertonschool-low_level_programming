e3aa233633abe7d715210a13aa9c49e952bc6a90
lhh108 <paintluis@gmail.com>
2019-09-24 17:03:09 +0000

    Adding alphabet description

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * abc - Continue point
- * Desc: Print alphabet
+ * print_alphabet - Continue point
+ * Desc: print_alphabet
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
