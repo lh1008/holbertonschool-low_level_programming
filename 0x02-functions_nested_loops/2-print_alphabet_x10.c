f26d790e4f752bc6541ca450d9d05dfe88d363fb
lhh108 <paintluis@gmail.com>
2019-09-24 18:27:29 +0000

    Printing 10 times while it loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_alphabet_10x - Continue point
+ * Desc: printing_alphabet_10x
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int abc = 0;
+	int az;
+
+	while (abc < 10)
+	{
+		for (az = 97; az < 123; az++)
+		{
+			_putchar(az);
+		}
+		_putchar(10);
+		abc++;
+	}
+}
