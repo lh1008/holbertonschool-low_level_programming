0b59cf91c27bf273768d830478c60b190270bf73
lhh108 <paintluis@gmail.com>
2019-09-25 01:17:52 +0000

    The longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * jack_bauer - Entry point
+ * Desc: jack_bauer
+ * @void: void
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+	int a, b;
+
+	for (a = 0; a < 24; a++)
+	{
+		for (b = 0; b < 60; b++)
+		{
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(58);
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar(10);
+		}
+	}
+}
