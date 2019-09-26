4683a3f1df5fe65e6365b68353851dd2bce865bc
lhh108 <paintluis@gmail.com>
2019-09-26 23:00:13 +0000

    Print Line Not finished

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_line - Entry point
+ * Desc: print_line
+ * @n: int type
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+
+	int x = '_';
+
+	if (n * x)
+	{
+		_putchar(x);
+	}
+	_putchar(0);
+}
