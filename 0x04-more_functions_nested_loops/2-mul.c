e1e18bfd2144dd3f1f831fbcfeed6bdce07b79c1
lhh108 <paintluis@gmail.com>
2019-09-26 18:27:46 +0000

    Multiply int mul important

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * mul - Continue point
+ * Desc: mul
+ * @a: int type
+ * @b: int type
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+
+	int mul = a * b;
+
+	return (mul);
+}
