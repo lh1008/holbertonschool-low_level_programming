41f74a9f60cbbd2511215b4b763aab6b6bfdd130
lh1008 <paintluis@gmail.com>
2019-09-29 15:50:01 -0500

    Adding two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * add - Entry point
+ * Desc: add
+ * @a: int type
+ * @b: int type
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+}
