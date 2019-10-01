8017d6f7b7325ccab6854199055f36ca33a865e7
lhh108 <paintluis@gmail.com>
2019-10-01 14:59:07 +0000

    Adding the correct file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * swap_int - Entry point
+ * Desc: swap_int
+ * @a: int type
+ * @b: int type
+ * Return: Always 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
