53157466714e5a5bf434a574386d1a41429dc6d7
lhh108 <paintluis@gmail.com>
2019-10-05 21:48:37 +0000

    Paranoid in revers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * reverse_array - Entry point
+ * Desc: reverse_array
+ * @a: int type
+ * @n: int type
+ * Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int b = 0, aux;
+
+	while (a[n] != '\0')
+	{
+		n++;
+	}
+	n--;
+	while (b < n)
+	{
+		aux = a[n - 1];
+		a[n - 1] = a[b];
+		a[b] = aux;
+		b++;
+		n--;
+	}
+}
