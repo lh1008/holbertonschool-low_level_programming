139d42b2e66016ae1053eb2a4d47ccb515efc0bb
lhh108 <paintluis@gmail.com>
2019-10-05 15:31:08 +0000

    When e lower than n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strncat - Entry point
+ * Desc: _strncat
+ * @dest: char type
+ * @src: char type
+ * Return: char (dest)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, e = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while ((src[e] != '\0') && (e < n))
+	{
+		dest[i] = src[e];
+		e++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
