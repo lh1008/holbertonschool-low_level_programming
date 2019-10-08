5f01dd177f6a49acb3fb51a1cc8b0eda7371ae60
lhh108 <paintluis@gmail.com>
2019-10-08 21:23:59 +0000

    Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * set_string - Entry point
+ * Desc: set_string pointer to a char
+ * @s: char type
+ * @to: char type
+ * Return: Function that sets the value of a pointer to a char.
+ **/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
