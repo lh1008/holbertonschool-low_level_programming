4865f6a1f5760e5027c79e6a8a9b3eab49a65cf8
lhh108 <paintluis@gmail.com>
2019-10-10 22:28:35 +0000

    Squares root are interested

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _sqrt_recursion - Entry point
+ * Desc: _sqrt_recursion
+ * @n: int type
+ * Return: Function that returns the natural square root of a number.
+ **/
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_recur(n, 1));
+}
+
+int _sqrt_recur(int n, int i)
+{
+	if (n <= 0)
+		return (-1);
+	if (i * i > n)
+		return (-1);
+	if (n == i * i)
+		return (i);
+	return (_sqrt_recur(n, i + 1));
+}
