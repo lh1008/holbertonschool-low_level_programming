1ca50d1c5d074bab7c93f247bdbac0e898023a73
lh1008 <paintluis@gmail.com>
2019-10-13 20:57:53 -0500

    Number of arguments is not always the first

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Desc: main function
+ * @argc: int type
+ * @argv: char type
+ * Return: Print the number of arguments
+ **/
+int main(int argc, char *argv[]__attribute__ ((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
