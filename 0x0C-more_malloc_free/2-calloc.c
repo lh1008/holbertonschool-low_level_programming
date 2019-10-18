ef51994a40f07258240b83a128399b5489e41664
lhh108 <paintluis@gmail.com>
2019-10-18 01:04:44 +0000

    Memory for an array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _calloc - Entry Point
+ * Desc: _calloc function
+ * @nmemb: unsigned int type
+ * @size: unsigned int type
+ * Return: Function that allocates memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+
+	if (P == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
+	return (p);
+}
