ff4249d81d39ce1834ee02da1a78897cbe91cc4f
lhh108 <paintluis@gmail.com>
2019-10-21 21:05:01 +0000

    Django_comment

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,13 @@
 
 int _putchar(char c);
 
+/**
+ * struct dog - New elements
+ * @name: char type member
+ * @age: float type member
+ * @owner: char type member
+ * Description: Define a new type with new elements
+ */
 struct dog
 {
 	char *name;
