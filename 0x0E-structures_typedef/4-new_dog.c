6c9cbdd478f6c4dca54dff3ff1be1d3be207ab18
lh1008 <paintluis@gmail.com>
2019-10-21 21:03:17 -0500

    Creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * new_dog - Entry Point
+ * Desc: new_dog function
+ * @name: name member type
+ * @age: age member type
+ * @owner: owner member type
+ * Return: Function that creates a new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *size;
+
+	size = malloc(sizeof(name) + sizeof(owner));
+	if (size == NULL)
+		return (NULL);
+	size->name = name;
+	size->age = age;
+	size->owner = owner;
+	return (size);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,7 +19,15 @@ struct dog
 
 typedef struct dog dog_t;
 
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+}size;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H */
