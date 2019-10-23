1cb7b2ae68ee77b916e72533e16ef10441ef0110
lhh108 <paintluis@gmail.com>
2019-10-23 18:31:03 +0000

    Missing NULL and stdio

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,16 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  * print_name - Entry Point
  * Desc: print_name function
  * @name: char type
  * @f: function
- * @*: char type
  * Return: Function that prints a name
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL || name == NULL)
+		return;
+
 	f(name);
 }
