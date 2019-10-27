fa8e9b7c6545848aa28a3710eb684abbbc9489ef
lhh108 <paintluis@gmail.com>
2019-10-27 21:58:33 +0000

    Value of variables

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,58 @@
-BB#include "variadic_functions.h"
+#include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+/**
+ * op_c - Entry Point
+ * Desc: op_c function
+ * @op_d: op_s from op_s
+ * Return: Char c
+ */
+void op_c(va_list op_d)
+{
+	printf("%c", va_arg(op_d, int));
+}
+
+/**
+ * op_i - Entry Point
+ * Desc: op_i function
+ * @op_d: op_i from op_s
+ * Return: Int i
+ */
+void op_i(va_list op_d)
+{
+	printf("%d", va_arg(op_d, int));
+}
+
+
+/**
+ * op_f - Entry Point
+ * Desc: op_f function
+ * @op_d: op_f from op_s
+ * Return: Float f - double
+ */
+void op_f(va_list op_d)
+{
+	printf("%f", va_arg(op_d, double));
+}
+
+/**
+ * op_nil - Entry Point
+ * Desc: op_nil function
+ * @op_d: op_nil from op_s
+ * Return: String
+ */
+void op_nil(va_list op_d)
+{
+	char *s = va_arg(op_d, char *);
+
+	if (s == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", s);
+}
+
 /**
  * print_all - Entry Point
  * Desc: print_all function
@@ -9,32 +61,36 @@ BB#include "variadic_functions.h"
  */
 void print_all(const char * const format, ...)
 {
-	va_list all;
-	int count;
 
-	va_start(all, format);
+	op_t op_s[] = {
+		{"c", op_c},
+		{"i", op_i},
+		{"f", op_f},
+		{"s", op_nil},
+		{NULL, NULL},
+	};
 
-	count = 0;
-	while (format != NULL)
+	va_list op_d;
+	unsigned int i = 0, j = 0;
+	char *o_o = "";
+
+	va_start(op_d, format);
+
+	while (format != NULL && format[i] != '\0')
 	{
-		if (s == NULL)
-			printf("(nil)");
-		switch (format)
+		j = 0;
+		while (op_s[j].op != NULL)
 		{
-		case c:
-			printf("%c", va_arg(all, char));
-			break;
-		case i:
-			printf("%d", va_arg(all, int));
-			break;
-		case f:
-			printf("%f", va_arg(all, float));
-			break;
-		case s:
-			printf("%s", va_arg(all, char *));
-			break;
+			if (format[i] == op_s[j].op[0])
+			{
+				printf("%s", o_o);
+				op_s[j].stargate(op_d);
+				o_o = ", ";
+			}
+			j++;
 		}
-		count += 1;
+		i++;
 	}
-	va_end(all);
+	printf("\n");
+	va_end(op_d);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,25 +1,22 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
+#include <stdarg.h>
+/**
+ * struct op_o - Struct fly_ops
+ * Desc: Flying operators
+ * @op: The operator
+ * @stargate: The function associated
+ */
+typedef struct op_o
+{
+	char *op;
+	void (*stargate)(va_list valist);
+} op_t;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-/**
- * struct format - New elements
- * @c: char type member
- * @i: int type member
- * @f: float type member
- * Description: Define format with new type with new elements
- */
-struct format
-{
-        char c;
-        int i;
-        float f;
-	char *s;
-};
 
 #endif /* FUNCTIONS_H */
