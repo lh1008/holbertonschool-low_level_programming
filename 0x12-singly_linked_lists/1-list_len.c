0f9dbb5846979c09886df74806f42365ac65b6ed
lhh108 <paintluis@gmail.com>
2019-10-31 21:21:48 +0000

    Return the number of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - Entry point
+ * Desc: list_len function
+ * @h: pointer to list_t
+ * Return: Function that returns the number of elements in a linked list_t list.
+ **/
+size_t list_len(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
