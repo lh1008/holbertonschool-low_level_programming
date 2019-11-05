88b63350c67a2688e80d8d6117d219549046eb5c
lhh108 <paintluis@gmail.com>
2019-11-05 18:49:40 +0000

    Adding node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_nodeint - Entry point
+ * Desc: add_nodeint function
+ * @head: pointer to list_t
+ * @n: const int
+ * Return: Function that adds a new node.
+ **/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *jar;
+
+	jar = malloc(sizeof(listint_t));
+
+	if (jar == NULL)
+	{
+		free (jar);
+		return (jar);
+	}
+	(*jar).n = n;
+	(*jar).next = *head;
+	*head = jar;
+	return (*head);
+}
