05f5ee6d30cc445902fcd5cfa6946e8154b7da75
lhh108 <paintluis@gmail.com>
2019-11-05 20:43:43 +0000

    Pop delete node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * pop_listint - Entry point
+ * Desc: pop_listint function
+ * @head: pointer of a pointer to listint_t
+ * Return: Function that deletes the head node.
+ **/
+int pop_listint(listint_t **head)
+{
+	listint_t *jar;
+	int var;
+
+	var = (*(*head)).n;
+
+	if (*head == NULL)
+		return (0);
+
+	jar = *head;
+	*head = ((*(*head)).next);
+	free(jar);
+	return (var);
+}
