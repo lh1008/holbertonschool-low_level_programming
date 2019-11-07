d6210802a8fc555227d239d04641ff8d6b1bbfd9
lhh108 <paintluis@gmail.com>
2019-11-07 15:15:18 +0000

    insert node, descrease counter

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 		}
 		actual = (*actual).next;
-		idx++;
+		idx--;
 	}
 	next = (*actual).next;
 	(*actual).next = target;
