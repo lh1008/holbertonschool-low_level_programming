8bce2ef965279e3de96a27afc7770fb6054f9e85
lhh108 <paintluis@gmail.com>
2019-11-07 18:31:16 +0000

    Adding delete node incomplete

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * delete_nodeint_at_index - Entry point
+ * Desc: delete_nodeint_at_index function
+ * @head: pointer of a pointer to listint_t
+ * @index: unsigned int index
+ * Return: Function that deletes a new node
+ **/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
