d82a35c97e9b878e6ee4323aeb556ec141482a0c
lh1008 <paintluis@gmail.com>
2019-11-07 23:17:48 -0500

    Binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * binary_to_uint - Entry point
  * Desc: binary_to_uint function
@@ -7,3 +8,23 @@
  **/
 unsigned int binary_to_uint(const char *b)
 {
+	int i = 0, rem, sum = 0, x = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		i++;
+	}
+	while (i > 0)
+	{
+		rem = i % 10;
+		sum += rem * _power(2, x);
+		i = i / 10;
+		i--;
+	}
+
+	return (sum);
+}
