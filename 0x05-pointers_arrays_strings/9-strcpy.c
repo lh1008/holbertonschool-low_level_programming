09467d8bbbfc0f4d4bd642694043192f1ea630a5
lhh108 <paintluis@gmail.com>
2019-11-11 15:22:54 +0000

    Adding if i > 0

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,13 +8,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	size_t i;
 
 	if (src != '\0')
 	{
 		for (i = 0; src[i] != '\0'; i++)
 			dest[i] = src[i];
-		dest[i + 1] = '\0';
+		if (i > 0)
+			dest[i + 1] = '\0';
 	}
 	return (dest);
 }
