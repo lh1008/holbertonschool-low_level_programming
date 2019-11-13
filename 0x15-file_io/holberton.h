68f55fc448c14c91e6e5dec3e7ef52104965b334
lhh108 <paintluis@gmail.com>
2019-11-13 22:27:32 +0000

    cp file_from file_to

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -4,6 +4,11 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
