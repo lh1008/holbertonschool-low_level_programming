3125f45209ebf38f6139478f1f1f229e2f636e70
lh1008 <paintluis@gmail.com>
2020-01-19 12:35:21 -0500

    Index > than size

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,6 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (0);
 		new_node->key = (char *)key;
 		new_node->value = strdup(value);
+		new_node->next = NULL;
 		array[index] = new_node;
 	}
 	else
