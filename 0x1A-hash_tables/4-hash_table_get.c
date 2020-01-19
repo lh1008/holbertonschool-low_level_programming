be2ec3c824bc584e24a2b251eeb33027f14626c2
lh1008 <paintluis@gmail.com>
2020-01-19 14:46:15 -0500

    4. >>> ht['betty']

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Entry point
+ * Desc: hash_table_get function
+ * @ht: hash_table_t
+ * @key: const char
+ * Return: Function that retrieves a value associated with a key.
+ **/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node)
+	{
+		if (strcmp(key, (const char *) (node->key)) == 0)
+		{
+			return (node->value);
+		}
+	}
+	return (NULL);
+}
