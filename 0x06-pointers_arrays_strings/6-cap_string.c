03661c2faf26acb0461219162483f44a080f08a6
lh1008 <paintluis@gmail.com>
2020-02-17 17:00:05 -0500

    Prepare for the worst

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * cap_string - Entry point
+ * Desc: cap_string function
+ * Return: Function that capitalizes all words of a string.
+ **/
+char *cap_string(char *)
+{
+	
+}
