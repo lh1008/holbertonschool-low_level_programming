c512471c2d456781b4e3bef05374bcd82f55e581
lh008 <paintluis@gmail.com>
2020-05-10 19:17:39 -0500

    Expect the best, Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,7 +5,22 @@
  * Desc: cap_string function
  * Return: Function that capitalizes all words of a string.
  **/
-char *cap_string(char *)
+char *cap_string(char *c)
 {
-	
+	int i = 0;
+
+	if (c[i] >= 97 && c[i] <= 122)
+		c[i] = c[i] - 32;
+
+	while (c[i] != '\0')
+	{
+		if ((c[i - 1] == ' ' || c[i - 1] == '\t' || c[i - 1] == '\n' ||
+		     c[i - 1] == ',' || c[i - 1] == ';' || c[i - 1] == '.' ||
+		     c[i - 1] == '!' || c[i - 1] == '?' || c[i - 1] == '"' ||
+		     c[i - 1] == '(' || c[i - 1] == ')' || c[i - 1] == '{' ||
+		     c[i - 1] == '}') && (c[i] >= 'a' && c[i] <= 'z'))
+			c[i] = c[i] - 32;
+		i++;
+	}
+	return (c);
 }
