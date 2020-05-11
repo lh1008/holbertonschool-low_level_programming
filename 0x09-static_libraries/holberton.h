40175f3fe74bd751536f5759bb1efb035bbbe22e
lh008 <paintluis@gmail.com>
2020-05-11 06:11:36 -0500

    Missing stdio library and endif comment, Task 0

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,6 +1,8 @@
 #ifndef HOLBERTON_FUNC
 #define HOLBERTON_FUNC
 
+#include <stdio.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,4 +24,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* HOLBERTON_FUNC */
