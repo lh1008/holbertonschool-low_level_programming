a147f3789a4aa1c15798c2dd91f791a75dc64831
lh008 <paintluis@gmail.com>
2020-05-12 07:07:19 -0500

    A new dog fixed for's, Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,43 +13,34 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *size;
 	char *box_name, *box_owner;
-	int n_l = 0, o_l = 0;
+	int n_l = 0, o_l = 0, j;
 
 	if (name == NULL && owner == NULL)
 		return (NULL);
-
 	size = malloc(sizeof(struct dog));
 	if (size == NULL)
-	{
-		free(size);
 		return (NULL);
-	}
-
 	while (name[n_l] != '\0')
 		n_l++;
-
-	box_name = malloc(sizeof(char) * (n_l + 1));
+	box_name = malloc(sizeof(char) * n_l + 1);
 	if (box_name == NULL)
 	{
 		free(size);
 		return (NULL);
 	}
-
 	while (owner[o_l] != '\0')
 		o_l++;
-
-	box_owner = malloc(sizeof(char) * (o_l + 1));
+	box_owner = malloc(sizeof(char) * o_l + 1);
 	if (box_owner == NULL)
 	{
 		free(size);
 		free(box_name);
 		return (NULL);
 	}
-	for (n_l = 0; name[n_l] != '\0'; n_l++)
-		box_name[n_l] = name[n_l];
-	for (o_l = 0; owner[o_l] != '\0'; o_l++)
-		box_owner[o_l] = owner[o_l];
-
+	for (j = 0; j <= n_l; j++)
+		box_name[j] = name[j];
+	for (j = 0; j <= o_l; j++)
+		box_owner[j] = owner[j];
 	size->name = box_name;
 	size->age = age;
 	size->owner = box_owner;
